76a877ffbca4b379f5280efa08a05b517f8d72b6
Solomon Ionut <i.solomon18@hotmail.com>
2023-07-14 16:30:26 +0300

    FEAT: start adding button functionality

--- core/gui/push_button.hpp
+++ core/gui/push_button.hpp
@@ -1,11 +1,35 @@
 #ifndef _PUSH_BUTTON_HPP
 #define _PUSH_BUTTON_HPP
 
+#include "raylib-cpp.hpp"
+
+enum PushButtonState
+{
+    NORMAL = 0,
+    MOUSE_HOVER,
+    PRESSED
+};
+
 class PushButton
 {
     public:
         PushButton();
-        ~PushButton();
+        PushButton(Texture2D);
+        ~PushButton();  
+
+    private:
+        int _NUM_FRAMES;            // Number of frames in the texture
+        Sound* _fx;	                // Button sound
+        Texture2D* _texture;        // Button texture
+        float _frameHeight;         // Height of current texture frame
+        Rectangle _sourceRec;       // Button frame rectangle for drawing
+        Rectangle _bounds;          // Button bounds on screen
+        PushButtonState _state;	    // Button state: 0-NORMAL, 1-MOUSE_HOVER, 2-PRESSED
+        bool _action;				// Button action should be activated
+        std::string _text;          // Button display text
+        int _textOffset;            // Text Y offset of the button
+        bool _isInsideContainer;    // Check if is inside a container or not
+        bool _hovering;             // True if mouse hovering over button, False otherwise
 };
 
 #endif // _PUSH_BUTTON_HPP
