f03d5fc2ae66b6dda3d5c654576a709b10c0cf30
Solomon Ionut <i.solomon18@hotmail.com>
2023-07-14 18:05:45 +0300

    FEAT: load textures for buttons

--- core/gui/push_button.hpp
+++ core/gui/push_button.hpp
@@ -14,13 +14,18 @@ class PushButton
 {
     public:
         PushButton();
-        PushButton(Texture2D);
+        PushButton(std::string texturePath, std::string fxPath, std::string fontPath);
         ~PushButton();  
 
     private:
+        std::string _texturePath;    // Button texture file path
+        std::string _fxPath;         // Button sound file path
+        std::string _fontPath;       // Button font file path
+
         int _NUM_FRAMES;            // Number of frames in the texture
-        Sound* _fx;	                // Button sound
-        Texture2D* _texture;        // Button texture
+        Texture2D _texture;        // Button texture
+        Sound _fx;	                // Button sound
+        Font _font;                 // Button font
         float _frameHeight;         // Height of current texture frame
         Rectangle _sourceRec;       // Button frame rectangle for drawing
         Rectangle _bounds;          // Button bounds on screen
@@ -28,7 +33,6 @@ class PushButton
         bool _action;				// Button action should be activated
         std::string _text;          // Button display text
         int _textOffset;            // Text Y offset of the button
-        bool _isInsideContainer;    // Check if is inside a container or not
         bool _hovering;             // True if mouse hovering over button, False otherwise
 };
 
