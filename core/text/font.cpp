b28fb858cb20c1b0039eac88b6f283c84601c054
Solomon Ionut <i.solomon18@hotmail.com>
2023-08-02 23:35:38 +0300

    FEAT: add font component for later use

---
+++ core/text/font.cpp
@@ -0,0 +1,13 @@
+#include "font.hpp"
+
+using namespace core::text;
+
+Font::Font()
+{
+
+};
+
+Font::~Font()
+{
+
+};
---
+++ core/text/font.hpp
@@ -0,0 +1,14 @@
+#pramga once
+
+#include "raylib-cpp.hpp"
+
+namespace core {
+    namespace text {
+        class Font
+        {
+            public:
+                Font();
+                ~Font();
+        };
+    }  // namespace text
+} // namespace core
